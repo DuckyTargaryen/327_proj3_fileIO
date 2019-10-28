e3a576cf8864dc60ded57ec647585fba04137452
Sean LaRochelle <sean.larochelle.17@cnu.edu>
2019-10-27 20:28:45 -0400

    Wrote out the method definitions for FileIO.

--- FileIO.cpp
+++ FileIO.cpp
@@ -13,12 +13,35 @@ using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	fstream inputFile;
+	inputFile.open(filename.c_str());
+	if(!inputFile.is_open()){
+		contents = "";
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+	string input;
+	while(!inputFile.eof()){
+		getline(inputFile, input, '\n');
+		contents += input;
+	}
+	cout << contents << endl;
+	inputFile.close();
+	return SUCCESS;
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream outputFile;
+	outputFile.open(filename.c_str());
+	if(!outputFile.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+	int length = myEntryVector.size();
+	for(int i = 0; i < length; i++){
+		outputFile << myEntryVector[i] << endl;
+	}
+	outputFile.close();
+	return SUCCESS;
 }
 
 
