a9593ae2a7083b84defd148c9fc0e02197a55384
Sean LaRochelle <sean.larochelle.17@cnu.edu>
2019-10-29 00:44:44 -0400

    Fixed an issue in the fileIO file.

--- FileIO.cpp
+++ FileIO.cpp
@@ -11,6 +11,12 @@
 
 using namespace std;
 
+/**
+ * Gets the contents from a file.
+ * @param filename - The name of the file being read
+ * @param contents - Contains the contents of the file
+ * @return int - Represents whether or not the file was successfully read
+ */
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
 	fstream inputFile;
@@ -24,17 +30,22 @@ int KP_FileIO::getFileContents(const std::string &filename, std::string &content
 		getline(inputFile, input, '\n');
 		contents += input;
 	}
-	cout << contents << endl;
 	inputFile.close();
 	return SUCCESS;
 }
 
+/**
+ * Writes the contents of myEntryVector to a file.
+ * @param filename - The file being writen to
+ * @param myEntryVector - The vector containing the contents
+ * @return int - Represents whether or not the file was successfully written to
+ */
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
 	ofstream outputFile;
 	outputFile.open(filename.c_str());
 	if(!outputFile.is_open()){
-		return COULD_NOT_OPEN_FILE_TO_READ;
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
 	}
 	int length = myEntryVector.size();
 	for(int i = 0; i < length; i++){
